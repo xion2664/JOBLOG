7d5f293cf5a78e19acc3675d486f12bc26521c7b
정휘원 <wonnyboi@naver.com>
2024-01-20 16:36:57 +0900

    [init] qna Board 레이아웃 세팅

---
+++ FrontEnd/src/views/community/qna_board/QnABoardView.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="qna-header">
+        <div class="header-left">
+            <h4>Q&A 게시판</h4>
+            <span>현직자와 취준생 간의 궁금한 이야기</span>
+        </div>
+        <div class="header-right">
+            <input type="text"> 검색기능 
+        </div>
+    </div>
+    <div class="qna-body">
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  .qna-header { 
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+
+  .header-left {
+
+  }
+
+  .qna-body {
+    display: grid;
+    grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+  }
+</style>
