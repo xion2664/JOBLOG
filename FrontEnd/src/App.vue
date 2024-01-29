2d40fa0ad00adbc465f428852ee39089e830e18f
xion <xion2664@gmail.com>
2024-01-29 17:31:24 +0900

    [rename] 스케줄 페이지 컴포넌트 배치 수정
    스케줄 페이지에 몰려있던 캘린더와 작업 목록을 컴포넌트로 분류하여 배치했습니다.

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <NavBar />
-  <div class="main-content">
+  <main>
     <RouterView />
-  </div>
+  </main>
   <footer>
     <img src="" alt="" />
   </footer>
@@ -14,30 +14,15 @@ import NavBar from "./views/home/components/NavBar.vue";
 </script>
 
 <style>
-body {
-  min-width: 1920px;
-}
-
-header,
+/* header,
 .main-content {
   width: 1920px;
   margin: 0 auto;
   padding: 0px 288px;
   box-sizing: border-box;
   border-bottom: 1px solid #d9d9d9;
-}
-
-header {
-  display: flex;
-  height: 100px;
-  align-items: center;
-  justify-content: center;
-}
-
-.main-content {
-  width: 70%;
   display: flex;
   flex-direction: column;
   align-items: center;
-}
+} */
 </style>
--- FrontEnd/src/views/home/HomeView.vue
+++ FrontEnd/src/views/home/HomeView.vue
@@ -2,41 +2,41 @@
   <!-- 로그인 시 화면 전환되는 메인 페이지를 구현하기 위한 버튼 -->
   <button @click="toggleLogin">로그인 상태 전환인 척하는 버튼</button>
 
-  <div v-if="!isLoggedIn">
-    <div class="main-img-container">
-      <img
-        src="../../assets/img/home/main-img-1.png"
-        alt="main img"
-        class="main-img"
-      />
+  <div class="container-visitor" v-if="!isLoggedIn">
+    <div class="welcome-pharse">
+      <div class="welcome-title">
+        <p class="bold">나의 첫 취업준비,</p>
+        <p class="black">JOBLOG.</p>
+      </div>
+      <div class="welcome-txt">
+        JOBLOG에서 <br />
+        나의 <b>취준이력</b>과 <b>스케줄 관리</b>, <br />
+        <b>현직자와의 커피챗</b>까지 <br />
+        경험해보세요.
+      </div>
     </div>
 
     <RouterLink :to="{ name: 'Login' }">
-      <img
-        src="../../assets/img/home/main-login-img.png"
-        alt="loginbutton"
-        class="login-button-img"
-      />
+      <button class="login-btn-solid clickable">
+        SNS 계정으로 시작하기 ->
+      </button>
     </RouterLink>
   </div>
 
-  <div v-else class="logged-in-content">
-      <p>로그인 시 표시할 화면</p>
-      <Calendar/>
-      <ResumeEssay/>
-      <ResumeEssay2/>
-      <Chatter/>
+  <div class="container-member" v-else>
+    <p>로그인 시 표시할 화면</p>
+    <Calendar />
+    <ResumeEssay />
+    <ResumeEssay2 />
+    <Chatter />
   </div>
-
-  <!-- Toggle Button -->
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import Calendar from './components/Calendar.vue';
-import ResumeEssay from './components/ResumeEssay.vue';
-// import ResumeEssay2 from './components/ResumeEssay2.vue';
-import Chatter from './components/Chatter.vue'
+import { ref } from "vue";
+import Calendar from "./components/Calendar.vue";
+import ResumeEssay from "./components/ResumeEssay.vue";
+import Chatter from "./components/Chatter.vue";
 // 로그인/로그아웃 상태를 대체하는 버튼 및 화면 전환용 로직
 // 나중에는 isAuthenticated나, token값 등을 사용하여 버튼 없이 자동 전환
 const isLoggedIn = ref(false);
@@ -48,8 +48,17 @@ function toggleLogin() {
 </script>
 
 <style scoped>
-button {
-  margin: 20px;
+.container {
+  background-image: url("@/assets/img/home/background-img.png");
+  background-size: cover;
+  background-position: center;
+  position: relative;
+  width: 100%;
+  height: 880px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
 }
 .main-img-container {
   display: flex;
@@ -66,14 +75,19 @@ button {
   margin: 0px;
   padding: 0px;
 }
-.login-button-img {
-  position: relative;
-  top: -400px;
-  left: 73%;
-  transform: translate(-50%, -50%);
+.login-btn-solid {
+  color: white;
+  text-align: center;
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  background-color: var(--main-blue);
+  border: var(--main-blue) 2px solid;
+  padding: 30px 50px;
   border-radius: 8px;
 }
-.logged-in-content {
+.container-members {
   width: 1344px;
 }
 </style>
