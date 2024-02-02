2852babb9f710812910c1e70f7b76a5fc14f349e
정휘원 <wonnyboi@naver.com>
2024-02-02 15:04:07 +0900

    [GIT] 백엔드 파일 삭제

--- backend/joblog/src/main/java/com/ssafy/joblog/domain/user/repository/TokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.joblog.domain.user.repository;
-
-import com.ssafy.joblog.domain.user.entity.Token;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TokenRepository extends JpaRepository<Token, Integer>  {
-
-}
