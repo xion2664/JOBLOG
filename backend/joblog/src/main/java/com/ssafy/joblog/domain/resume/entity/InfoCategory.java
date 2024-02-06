54ff3b104af4f21cdd042d417f6e66c55fd0f6df
hyun0-25 <lisa1072@skku.edu>
2024-02-06 17:12:27 +0900

    [fix] 정보 info 엔티티 카테고리 축소화
    erd 참고
    
    #491

--- backend/joblog/src/main/java/com/ssafy/joblog/domain/resume/entity/InfoCategory.java
+++ backend/joblog/src/main/java/com/ssafy/joblog/domain/resume/entity/InfoCategory.java
@@ -2,7 +2,7 @@ package com.ssafy.joblog.domain.resume.entity;
 
 public enum InfoCategory {
     // 학력, 교내외활동, 경력, 자격증, 어학
-    EDUCATION, ACTIVITY, CAREER, CERTIFICATE, LANGUAGE_ABILITY,
+    EDUCATION, ACTIVITY, CAREER, CERTIFICATE,
     // 수상, 프로젝트,
-    AWARD, PROJECT, TRAINING, SKILL;
+    AWARD, SKILL;
 }
