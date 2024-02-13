dd5a5e6f024fc235b9e48676675bcfc6bc22cdb4
hyun0-25 <lisa1072@skku.edu>
2024-02-13 17:57:14 +0900

    [feat] AWS S3 프로필 사진 업로드
    aws s3 연동해서 사진 업로드 구현 완료
    
    #558

--- backend/joblog/src/main/java/com/ssafy/joblog/domain/user/controller/UserController.java
+++ backend/joblog/src/main/java/com/ssafy/joblog/domain/user/controller/UserController.java
@@ -1,20 +1,49 @@
 package com.ssafy.joblog.domain.user.controller;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.ssafy.joblog.domain.user.dto.request.UserUpdateRequestDto;
 import com.ssafy.joblog.domain.user.dto.response.UserResponseDto;
 import com.ssafy.joblog.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @PostMapping("/upload/{userId}")
+    public ResponseEntity<String> uploadFile(
+            @PathVariable(value = "userId") int userId,
+            @RequestParam("file") MultipartFile file) {
+        try {
+            String fileName = file.getOriginalFilename();
+            String fileUrl = "https://" + bucket + "/test" + fileName;
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType(file.getContentType());
+            metadata.setContentLength(file.getSize());
+            amazonS3Client.putObject(bucket, fileName, file.getInputStream(), metadata);
+            return ResponseEntity.ok(fileUrl);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
 
     @GetMapping("/{userId}")
     public ResponseEntity<UserResponseDto> findUser(@PathVariable(value = "userId") int userId) {
