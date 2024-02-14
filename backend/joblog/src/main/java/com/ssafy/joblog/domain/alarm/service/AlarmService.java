08a912b00ad0a4c9158374b7ac1ff1c92c814ea6
이희병 <gmlquddl67@naver.com>
2024-02-14 15:05:57 +0900

    [fix] alarm

--- backend/joblog/src/main/java/com/ssafy/joblog/domain/alarm/service/AlarmService.java
+++ backend/joblog/src/main/java/com/ssafy/joblog/domain/alarm/service/AlarmService.java
@@ -80,8 +80,8 @@ public class AlarmService {
         producerService.chatRejectAlarm(userId, "커피챗 거절");
     }
 
-    @Scheduled(cron = "0 0 0 * * *")
-//    @Scheduled(cron = "0/10 * * * * *")
+//    @Scheduled(cron = "0 0 0 * * *")
+    @Scheduled(cron = "0/10 * * * * *")
     public void sendScheduleAlarm() {
         LocalDateTime today = LocalDateTime.now();
         LocalDateTime tomorrow = today.plusDays(1);
