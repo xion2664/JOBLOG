5e1bf89485264f7f1d9da024bb6045746d7a47ce
정휘원 <wonnyboi@naver.com>
2024-02-15 09:24:05 +0900

    [feat] diary 페이지네이션 수정, 작성 최대 자수 변경

--- FrontEnd/src/views/blog/journal/JournalUpdateView.vue
+++ FrontEnd/src/views/blog/journal/JournalUpdateView.vue
@@ -4,6 +4,7 @@ import { useRoute } from "vue-router";
 import axios from "axios";
 import { useAuthStore } from "@/stores/auth";
 import { useRouter } from "vue-router";
+import { formatISODate } from "@/utils/utility";
 
 const authStore = useAuthStore();
 const route = useRoute();
@@ -16,16 +17,14 @@ console.log(journal);
 async function fetchJournal() {
   try {
     await authStore.updateUserInfoFromToken();
-    const response = await axios.get(
-      `${authStore.API_URL}/diary/detail/${route.params.id}`
-    );
+    const response = await axios.get(`${authStore.API_URL}/diary/detail/${route.params.id}`);
 
     return {
-      post: response.data
+      post: response.data,
     };
   } catch (error) {
     console.error("불러오기 실패: ", error);
-    return { journal: null }; 
+    return { journal: null };
   }
 }
 
@@ -43,11 +42,7 @@ const saveJournal = async () => {
       content: journal.value.content,
     };
     console.log(journal.value.diaryId);
-    const res = await axios.patch(
-      `${authStore.API_URL}/diary/update`,
-      editContent,
-      config
-    );
+    const res = await axios.patch(`${authStore.API_URL}/diary/update`, editContent, config);
     console.log(res.data);
     router.push({ name: "JournalDetail", params: { id: journal.value.diaryId } });
   } catch (error) {
@@ -77,34 +72,22 @@ function toBack() {
           수정 취소
         </a>
         <h3>수정하기</h3>
-        <a
-          @click="saveJournal"
-          class="btn lined-bg f-color-c h-lined-c a-solid-c"
-        >
-          <i class='fa fa-floppy-o'></i>
+        <a @click="saveJournal" class="btn lined-bg f-color-c h-lined-c a-solid-c">
+          <i class="fa fa-floppy-o"></i>
           수정 완료
         </a>
       </div>
 
       <div class="content">
         <div class="title" v-if="journal">
-          <i class="fa-solid fa fa-pencil fa-2xl"></i>
-          <input
-            type="text"
-            class="input-simple focus-lined-c"
-            placeholder="글을 작성해주세요"
-            v-model="journal.content"
-            required
-          />
+          <div>
+            <i class="fa-solid fa fa-pencil fa-2xl"></i>
+            <b>{{ journal.createdDate }}</b>
+          </div>
+          <div>{{ journal.content.length }} 자</div>
         </div>
-        <textarea
-          class="input focus-lined-c"
-          placeholder="미리보기"
-          v-model="journal.content"
-          required
-        ></textarea>
+        <textarea class="input focus-lined-c" placeholder="미리보기" v-model="journal.content" required></textarea>
       </div>
-
     </div>
   </div>
 </template>
@@ -136,9 +119,16 @@ function toBack() {
 .title {
   display: flex;
   align-items: center;
+  justify-content: space-between;
   gap: 20px;
   padding: 20px 0;
+  box-sizing: border-box;
 }
+
+.title b {
+  font-size: 16pt;
+}
+
 .title input {
   width: 100%;
   font-size: 28px;
